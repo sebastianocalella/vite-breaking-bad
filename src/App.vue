1371f946ba698ae2057fa500977e3dd5505421fb
Sebastiano Calella <sebastianocalella97@gmail.com>
2023-01-11 15:55:24 +0100

    inizialize App.vue components

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
 <script>
+  import AppHeader from './components/AppHeader.vue';
+  import AppMain from './components/AppMain.vue';
 
+  export default{
+    components: {
+      AppHeader,
+      AppMain,
+    }
+  }
 </script>
 
 <template>
+  <header>
+    <AppHeader/>
+  </header>
 
+  <main>
+    <AppMain/>
+  </main>
 </template>
 
 <style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,16 @@
+<template lang="">
+    <div>
+        <h2>header</h2>
+    </div>
+</template>
+
+<script>
+
+export default {
+    
+}
+</script>
+
+<style lang="">
+    
+</style>
